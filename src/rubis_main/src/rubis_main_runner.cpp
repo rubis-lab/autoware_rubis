51b125be959d2f3177313a7b688c0a50c11a7b18
Youngeun Cho <upoque@gmail.com>
2021-04-20 18:49:07 +0900

    compiled

---
+++ src/rubis_main/src/rubis_main_runner.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char * argv[])
+{
+//   rclcpp::init(0, nullptr);
+//   std::cout << "hi!" << std::endl;
+  
+//   rclcpp::NodeOptions node_options;
+//   std::vector<std::thread> thrs;
+
+//   rclcpp::shutdown();
+  std::cout << "rubis_main_runner launched 222." << std::endl;
+  return 0;
+}
