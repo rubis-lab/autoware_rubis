97fdb32e93582119f23dd4bf12f4728afc53e865
Youngeun Cho <upoque@gmail.com>
2021-04-20 18:52:11 +0900

    depends

--- src/rubis_main/src/rubis_main_runner.cpp
+++ src/rubis_main/src/rubis_main_runner.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
+// ROS2 dependencies
+#include "rclcpp/executor.hpp"
+#include "rclcpp/rclcpp.hpp"
+
 int main(int argc, char * argv[])
 {
-//   rclcpp::init(0, nullptr);
-//   std::cout << "hi!" << std::endl;
+  rclcpp::init(0, nullptr);
+  std::cout << "hi!" << std::endl;
   
 //   rclcpp::NodeOptions node_options;
 //   std::vector<std::thread> thrs;
 
-//   rclcpp::shutdown();
+  rclcpp::shutdown();
   std::cout << "rubis_main_runner launched 222." << std::endl;
   return 0;
 }
