6c4c1d02efaac2710c9e83d16576d9b40df36361
Youngeun Cho <upoque@gmail.com>
2021-04-25 19:28:20 +0900

    main edit

--- src/rubis_main/src/rubis_main_runner_dev.cpp
+++ src/rubis_main/src/rubis_main_runner_dev.cpp
@@ -14,6 +14,41 @@
 
 using rclcpp::executors::SingleThreadedExecutor;
 using rubis::sched::set_sched_deadline;
+using autoware::perception::filters::point_cloud_filter_transform_nodes::PointCloud2FilterTransformNode;
+
+void routine_point_cloud_filter_transform_lidar_front_nodes() {
+  // node
+  auto ptr_point_cloud_filter_transform_lidar_front_nodes = 
+    std::make_shared<PointCloud2FilterTransformNode>(
+      "point_cloud_filter_transform_front", "lidar_front",
+      configure_point_cloud_filter_transform_lidar_front_nodes());
+
+  // executor
+  SingleThreadedExecutor exec_point_cloud_filter_transform_lidar_front_nodes;
+  exec_point_cloud_filter_transform_lidar_front_nodes.add_node(
+    ptr_point_cloud_filter_transform_lidar_front_nodes);
+
+  // routine
+  exec_point_cloud_filter_transform_lidar_front_nodes.spin();
+  return;
+}
+
+void routine_point_cloud_filter_transform_lidar_rear_nodes() {
+  auto ptr_point_cloud_filter_transform_lidar_rear_nodes =
+    std::make_shared<PointCloud2FilterTransformNode>(
+      "point_cloud_filter_transform_rear", "lidar_rear",
+      configure_point_cloud_filter_transform_lidar_rear_nodes());
+  SingleThreadedExecutor exec_point_cloud_filter_transform_lidar_rear_nodes;
+  exec_point_cloud_filter_transform_lidar_rear_nodes.add_node(
+    ptr_point_cloud_filter_transform_lidar_rear_nodes);
+
+  int tid = gettid();
+  std::cout << "(point_cloud_filter_transform_lidar_rear_nodes) tid: " << tid << std::endl;
+  set_sched_deadline(tid, 1000000, 2000000, 3000000);
+  exec_point_cloud_filter_transform_lidar_rear_nodes.spin();
+  std::cout << "point_cloud_filter_transform_lidar_rear_nodes" << std::endl; 
+  return;
+}
 
 int main(int argc, char * argv[])
 {
@@ -22,33 +57,10 @@ int main(int argc, char * argv[])
   rclcpp::init(0, nullptr);
   std::vector<std::thread> thrs;
 
-  // point_cloud_filter_transform_lidar_nodes
-  // point_cloud_filter_transform_lidar_front_nodes
-  using autoware::perception::filters::point_cloud_filter_transform_nodes::PointCloud2FilterTransformNode;
-  auto ptr_point_cloud_filter_transform_lidar_front_nodes = std::make_shared<PointCloud2FilterTransformNode>(
-    "point_cloud_filter_transform_front", "lidar_front", configure_point_cloud_filter_transform_lidar_front_nodes());
-  SingleThreadedExecutor exec_point_cloud_filter_transform_lidar_front_nodes;
-  exec_point_cloud_filter_transform_lidar_front_nodes.add_node(ptr_point_cloud_filter_transform_lidar_front_nodes);
-
-  thrs.emplace_back(std::thread([&](){
-    exec_point_cloud_filter_transform_lidar_front_nodes.spin();
-  }));
-  std::cout << "point_cloud_filter_transform_lidar_front_nodes" << std::endl;
-
-  // point_cloud_filter_transform_lidar_rear_nodes
-  using autoware::perception::filters::point_cloud_filter_transform_nodes::PointCloud2FilterTransformNode;
-  auto ptr_point_cloud_filter_transform_lidar_rear_nodes = std::make_shared<PointCloud2FilterTransformNode>(
-    "point_cloud_filter_transform_rear", "lidar_rear", configure_point_cloud_filter_transform_lidar_rear_nodes());
-  SingleThreadedExecutor exec_point_cloud_filter_transform_lidar_rear_nodes;
-  exec_point_cloud_filter_transform_lidar_rear_nodes.add_node(ptr_point_cloud_filter_transform_lidar_rear_nodes);
-
-  thrs.emplace_back(std::thread([&](){
-    int tid = gettid();
-    std::cout << "(point_cloud_filter_transform_lidar_rear_nodes) tid: " << tid << std::endl;
-    set_sched_deadline(tid, 1000000, 2000000, 3000000);
-    exec_point_cloud_filter_transform_lidar_rear_nodes.spin();
-  }));
-  std::cout << "point_cloud_filter_transform_lidar_rear_nodes" << std::endl;  
+  thrs.emplace_back(std::thread(
+    routine_point_cloud_filter_transform_lidar_front_nodes));
+  thrs.emplace_back(std::thread(
+    routine_point_cloud_filter_transform_lidar_rear_nodes));
 
   // main end
   for(int i = 0; i < thrs.size(); i++) {
